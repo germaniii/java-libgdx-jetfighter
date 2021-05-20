020a463cc6d2ef551e248d50128545d89354150d
German III <germaniiifelisarta@gmail.com>
2021-05-21 00:57:10 +0800

    v0.5 Added Game Object Variables

--- core/src/com/germaniii/jetfighter/gameobjects/Bullet.java
+++ core/src/com/germaniii/jetfighter/gameobjects/Bullet.java
@@ -1,4 +1,12 @@
 package com.germaniii.jetfighter.gameobjects;
 
 public class Bullet {
+    float rad;
+    float x, y;
+    float speedX, speedY;
+
+    Bullet(){
+
+    }
+
 }
--- core/src/com/germaniii/jetfighter/gameobjects/Fighter.java
+++ core/src/com/germaniii/jetfighter/gameobjects/Fighter.java
@@ -1,4 +1,11 @@
 package com.germaniii.jetfighter.gameobjects;
 
 public class Fighter {
+    float x, y;
+    float speedX, speedY;
+    float planeHeight, planeWidth;
+
+    Fighter(){
+
+    }
 }
