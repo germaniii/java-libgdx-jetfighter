9e69d23061e5218de5fd577eac8c588007a0282e
German III <germaniiifelisarta@gmail.com>
2021-05-21 00:03:19 +0800

    v0.1 Added Desktop Config

--- desktop/src/com/germaniii/jetfighter/desktop/DesktopLauncher.java
+++ desktop/src/com/germaniii/jetfighter/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.germaniii.jetfighter.JetFighter;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "German III's Jet Fighter";
+		config.height = 600;
+		config.width = 800;
 		new LwjglApplication(new JetFighter(), config);
 	}
 }
