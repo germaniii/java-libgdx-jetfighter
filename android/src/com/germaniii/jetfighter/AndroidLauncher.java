259dc19a8664aefac35e43af9dfc18cae611cbba
German III <germaniiifelisarta@gmail.com>
2021-05-20 23:59:36 +0800

    v0.0 Initial Commit

---
+++ android/src/com/germaniii/jetfighter/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.germaniii.jetfighter;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.germaniii.jetfighter.JetFighter;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new JetFighter(), config);
+	}
+}
